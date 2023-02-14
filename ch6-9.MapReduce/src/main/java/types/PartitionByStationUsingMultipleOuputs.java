2b6a98124798bba7989850b896bac1870b1c5e38
yujinii59 <wssj3259@naver.com>
2023-02-15 01:50:43 +0900

    study mapreduce input/output format

---
+++ ch6-9.MapReduce/src/main/java/types/PartitionByStationUsingMultipleOuputs.java
@@ -0,0 +1,70 @@
+package types;
+
+import common.JobBuilder;
+import common.NcdcRecordParser;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+public class PartitionByStationUsingMultipleOuputs extends Configured implements Tool {
+    static class StationMapper extends Mapper<LongWritable, Text, Text, Text> {
+        private NcdcRecordParser parser = new NcdcRecordParser();
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            parser.parse(value);
+            context.write(new Text(parser.getStationId()), value);
+        }
+
+    }
+    static class MultipleoutputsReducer extends Reducer<Text, Text, NullWritable, Text> {
+        private MultipleOutputs<NullWritable, Text> multipleOutputs;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            multipleOutputs = new MultipleOutputs<NullWritable, Text>(context);
+        }
+
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            for (Text value: values) {
+                // key, value, base path -> base_path + key + value 형태로 파일 생성
+                multipleOutputs.write(NullWritable.get(), value, key.toString());
+            }
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            multipleOutputs.close();
+        }
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = JobBuilder.parseInputAndOutput(this, getConf(), args);
+        if (job == null) {
+            return -1;
+        }
+        job.setMapperClass(StationMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setReducerClass(MultipleoutputsReducer.class);
+        job.setOutputKeyClass(NullWritable.class);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception{
+        int exitCode = ToolRunner.run(new PartitionByStationUsingMultipleOuputs(), args);
+        System.exit(exitCode);
+    }
+
+}
